531714808b644953a8ef5ee8f4d4a9b72c9c65ea
julian gil gonzalez <34283674+juliangilg@users.noreply.github.com>
2026-03-17 09:18:58 -0500

    Set reference value for missing Y entries
    Assign reference value for missing data in Y array.

--- utils.py
+++ utils.py
@@ -135,6 +135,7 @@ def multiple_annotators(R, NrP, Xtrain, ytrain):
 
   # Finally, if iAnn=0 we assign a reference value to indicate a missing value
   Vref = -1e-20
+  Y[iAnn] = Vref
 
   return Lam_r, Ytrain, iAnn, Vref
 
